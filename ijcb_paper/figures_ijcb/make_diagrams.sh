65f0ee3259d1b0c2a7611f43194c4ee0f0ef0b07
Drew Wagner <drewm1980@gmail.com>
2011-06-13 14:39:07 -0500

    Converting more figures to pdf.

--- ijcb_paper/figures_ijcb/make_diagrams.sh
+++ ijcb_paper/figures_ijcb/make_diagrams.sh
@@ -5,3 +5,17 @@ dia pipeline_simplified.dia -e pipeline_simplified.tex -t tex
 latexmk -pdfdvi pipeline_simplified_wrapper.tex
 mv pipeline_simplified_wrapper.pdf pipeline_simplified.pdf
 pdfcrop --margins "0" pipeline_simplified.pdf pipeline_simplified.pdf
+
+# convert some stuff from eps to pdf.  Some of these could be done directly from inkscape.
+#epstopdf arrays.eps
+#epstopdf cpu_caches.eps
+#epstopdf gpu_caches.eps
+epstopdf time_vs_matrix_size_constant_tol.eps
+epstopdf alignment_runtime_graph.eps
+epstopdf speedVsResolution.eps
+epstopdf accuracyVsResolution.eps
+
+
+
+
+
